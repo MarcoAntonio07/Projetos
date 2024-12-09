16ba71dc4619321b49e25ff8f408af38c2270275
Marco Antonio <marcoa.amaraldasilva@gmail.com>
2024-12-09 16:30:50 -0300

    atualizações

---
+++ projeto de estudo/script2.js
@@ -0,0 +1,13 @@
+let nome = "Marcela"
+
+switch (nome) {
+    case "Marco":
+    console.log("Seu nome é Marco")
+    break
+    case "Marcela":
+    console.log("Seu nome é Marcela")
+    break
+    case "Ana":
+    console.log("Seu nome é Ana")
+    break
+}
